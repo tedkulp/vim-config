921183a05a8bf1947bb8f70114946222a50341e1
Ted Kulp <ted@tedkulp.com>
2012-04-18 15:56:08 -0400

    Updated setup script to make it chef friendly

--- setup.sh
+++ setup.sh
@@ -2,16 +2,19 @@
 
 PWD=`pwd`
 
-ln -ivs $PWD/.vim ~/.vim
-ln -ivs $PWD/.vimrc ~/.vimrc
-ln -ivs $PWD/.gvimrc ~/.gvimrc
+rm ~/.vim
+ln -s $PWD/.vim ~/.vim
+rm ~/.vimrc
+ln -s $PWD/.vimrc ~/.vimrc
+rm ~/.gvimrc
+ln -s $PWD/.gvimrc ~/.gvimrc
 
 if [ -d "$PWD/.vim/vundle.git" ]; then
   cd $PWD/.vim/vundle.git
   git pull origin master
   cd $PWD
-  vim +BundleInstall! +qa
+  vim -c BundleInstall! -c qall > /dev/null 2>&1
 else
   git clone http://github.com/gmarik/vundle.git $PWD/.vim/vundle.git
-  vim +BundleInstall +qa
+  vim -c BundleInstall -c qall > /dev/null 2>&1
 fi
